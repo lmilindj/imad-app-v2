aa3c7a23a413a79a183563b67b17edd00c911c4f
lmilindj <milindlalwani1997@gmail.com>
2017-02-28 22:46:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/ui/artical-one', function (req, res) {
+  res.send("Artical one requested : will be served");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
