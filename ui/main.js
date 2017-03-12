84e333046b97c8ea80228180a876cd1700a0d414
lmilindj <milindlalwani1997@gmail.com>
2017-03-12 16:49:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ var img=document.getElementById("madi");
 var leftMargin = 0;
 function ch(){
     leftMargin += 1;
-    img.style.leftMargin = leftMargin + "px";
+    img.style.marginLeft = leftMargin + "px";
 }
 img.onclick=function(){
     var interval = setInterval(ch,50);
